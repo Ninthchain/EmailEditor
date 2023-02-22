5c69ef5bf0be2e71a9add53412b36d7ca9a1a87c
Артём <73429297+Ninthchain@users.noreply.github.com>
2023-02-22 18:45:45 +0300

    Done

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const ExcelJs = require('exceljs');
 const clc = require('cli-color')
-const { abort, exit } = require('process');
+const { abort } = require('process');
 const FS = require('fs')
-const { isStringObject } = require('util/types');
-const { notEqual } = require('assert');
-const { isNull } = require('util');
 
-const banWords = [
+const banDomains = [
     'noreply', 
     'mail', 
     'post', 
@@ -24,54 +21,79 @@ const banWords = [
  * @param {String} email
  * @return {Boolean}
  */
-function isIncorrectEmail(email)  {
+function isCorrectEmail(email)  {
     if(email == null) return;
-    for(let i = 0 ; i < banWords.length; i++) {
+    for(let slotIt = 0 ; slotIt < banDomains.length; slotIt++) {
         
-        if(email.includes(banWords[i])) return true;
+        if(email.includes(banDomains[slotIt])) return false;
     }
-    return false;
+    return true;
 }
-console.log(clc.cyan('Делаем копию excel файла...'))
+
+
+
+function logError(str) {
+    console.log(clc.red(str))
+    abort()
+}
+function logComment(str) { 
+    console.log(clc.cyan(str))
+}
+function logWarning(str) {
+    console.log(clc.yellowBright(str))
+}
+function logSuccess(str) {
+    console.log(clc.green(str))
+}
+
+logComment('Copying original file..\n')
 try {
+    logComment('Copying started')
     FS.copyFileSync('./input.xlsx', 'inputCopy.xlsx')
 }
 catch(err) {
-    console.error(clc.red('Ошибка в копировании'));
-    abort()
+    logError('Error while copying')
 } 
-console.log(clc.green('Копия сделана\n'))
+logSuccess('Copy done!')
 
-// email columns from 4th index to 15th number;
+logComment('Editing started')
+
 let workbook = new ExcelJs.Workbook();
-  workbook.xlsx.readFile('./input.xlsx').then(async function(wb){
-    const workAdressesColumn = 6;
-    wb.eachSheet((sheet, id) => {
 
-      sheet.eachRow({includeEmpty: false}, async (row, number) => {
-        if(isIncorrectEmail(row.getCell(workAdressesColumn).value)) {
-            console.log(clc.cyanBright(`Обнаружен адрес эл почты с запрещеным доменом.\n\tАдрес:${row.getCell(workAdressesColumn).value} Позиция ячейки: ${row.getCell(workAdressesColumn).$col$row}`))
-            console.log(clc.green(`Ищу доступный дополнительный адрес`))
-            var previousValue = row.getCell(workAdressesColumn).value
+workbook.xlsx.readFile('./input.xlsx').then(async function(wb) {
+    
+    const workAdressesColumn = 6;
+    wb.eachSheet((sheet) => {
+
+      sheet.eachRow({includeEmpty: false}, async (row) => {
+        
+        if(!isCorrectEmail(row.getCell(workAdressesColumn).value)) {
+
             var changed = false;
-            for(let i = 7; i != 15; i++) {
-                if(row.getCell(i).value == null) return;
-                if(row.getCell(i).value.length > 0) {
-                    if(isIncorrectEmail(row.getCell(i).value)) continue;
+            
+            logComment(`Found email with banned domain.\n\ Address:${row.getCell(workAdressesColumn).value} Excel position: ${row.getCell(workAdressesColumn).$col$row}`)
+            logComment('Trying to find additional address')
+            
+            for(let slotIt = 7; slotIt != 15; slotIt++) {
+
+                if(row.getCell(slotIt).value == null) return;
+
+                if(row.getCell(slotIt).value.length > 0) {
+                    
+                    if(!isCorrectEmail(row.getCell(slotIt).value)) continue;
                         try {
-                            row.getCell(workAdressesColumn).value = row.getCell(i).value
+                            row.getCell(workAdressesColumn).value = row.getCell(slotIt).value
                         }
                         catch(err) {
-                            console.error(clc.red(err));
-                            abort()
+                            logError(err)
                         }}
-                        console.log(clc.green('Адрес ') + clc.whiteBright(previousValue) + clc.green(' заменён успешно на ') + clc.whiteBright(row.getCell(i).value))
+                        
+                        logSuccess(`The email has successfully changed to ${row.getCell(slotIt).value}`)
                         changed = true
                         break;
                 }
-            }
-            if(!changed) console.warn(clc.red(`Адрес эл почты ${row.getCell(workAdressesColumn).value} неудалось заменить. Нету дополнительных адресов для замены`))
+            } if(!changed) logWarning(`The email ${row.getCell(workAdressesColumn).value} have not been replaced. Dont have any right addresses`)
         })
       })
-      return workbook.xlsx.writeFile('output.xlsx')
-    })
+    return workbook.xlsx.writeFile('output.xlsx')
+})
